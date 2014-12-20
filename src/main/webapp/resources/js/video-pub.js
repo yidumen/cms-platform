9b254845fd2acd3968c4ca5b5828d3ba6eb771c4
pholance <pholance@gmail.com>
2014-12-20 21:44:48 +0800

    20141220 21:44

--- src/main/webapp/resources/js/video-pub.js
+++ src/main/webapp/resources/js/video-pub.js
@@ -9,31 +9,3 @@ $("#pub-shoot").datetimepicker({
     todayHighlight: true,
     forceParse: false
 });
-$("form").submit(function () {
-    $.ajax({
-        data: $(this).serialize(),
-        type: "post",
-        url: $(this).attr('action'),
-        success: function (data) {
-            if (data === "0") {
-                var pattern = /([0-9]*)-([0-9]*)-([0-9]*)/;
-                var date = $("#pub-shoot").val();
-                var matches = date.match(pattern);
-                var content = "@echo off\r\nset video_id=" + $("#file").val() + "\r\nset title=" + $("#title").val() + "\r\nset video_year=" + matches[1] + "\r\nset video_month=" + matches[2] + "\r\nset video_day=" + matches[3] + "\r\ncall X:\\ChatRoom\\Tools\\video_opening_title_generator.bat";
-                downloadFile("cms.bat", content);
-            } else {
-                showError(data);
-            }
-        }
-    });
-    return false;
-});
-function downloadFile(fileName, content) {
-    var aLink = document.createElement('a');
-    var blob = new Blob([content]);
-    var evt = document.createEvent("HTMLEvents");
-    evt.initEvent("click", false, false);//initEvent 不加后两个参数在FF下会报错, 感谢 Barret Lee 的反馈
-    aLink.download = fileName;
-    aLink.href = URL.createObjectURL(blob);
-    aLink.dispatchEvent(evt);
-}
