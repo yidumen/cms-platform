ec345312df40803a0cb1d6d1f8a09ba39836fdb5
pholance <pholance@gmail.com>
2015-07-03 10:55:17 +0800

    修复视频不能发布的问题

--- src/main/java/com/yidumen/cms/service/impl/Util.java
+++ src/main/java/com/yidumen/cms/service/impl/Util.java
@@ -4,33 +4,18 @@ import com.aliyun.openservices.ClientException;
 import com.aliyun.openservices.oss.OSSClient;
 import com.aliyun.openservices.oss.OSSException;
 import com.aliyun.openservices.oss.model.OSSObject;
-import com.aliyun.openservices.oss.model.ObjectMetadata;
 import com.jfinal.kit.PropKit;
-import com.yidumen.cms.service.exception.IllDataException;
-import org.apache.http.HttpHost;
-import org.apache.http.HttpResponse;
-import org.apache.http.auth.AuthScope;
-import org.apache.http.auth.UsernamePasswordCredentials;
-import org.apache.http.client.CredentialsProvider;
-import org.apache.http.client.methods.HttpGet;
-import org.apache.http.conn.params.ConnRouteParams;
-import org.apache.http.impl.client.BasicCredentialsProvider;
-import org.apache.http.impl.client.DefaultHttpClient;
-
-import java.io.IOException;
 
 /**
- *
  * @author 蔡迪旻
  */
 public final class Util {
 
-    private final static OSSClient client = new OSSClient(PropKit.get("oss.ydm.endpoint"),
-            PropKit.get("oss.ydm.accessKeyId"),
-            PropKit.get("oss.ydm.accessKeySecret"));
-    private final static String bucketName = PropKit.get("oss.ydm.bucketName");
-
     public static boolean isOSSFileExist(String key) throws ClientException {
+        final OSSClient client = new OSSClient(PropKit.get("oss.ydm.endpoint"),
+                PropKit.get("oss.ydm.accessKeyId"),
+                PropKit.get("oss.ydm.accessKeySecret"));
+        final String bucketName = PropKit.get("oss.ydm.bucketName");
         try {
             final OSSObject object = client.getObject(bucketName, key);
             if (object != null) {
@@ -39,7 +24,7 @@ public final class Util {
         } catch (OSSException e) {
             return false;
         } catch (ClientException e) {
-           throw e;
+            throw e;
         }
         return false;
     }
